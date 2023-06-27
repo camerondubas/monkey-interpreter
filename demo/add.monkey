b4a381efa9c2cc048e7723cf0559229d133ae575
Cameron Dubas <camerondubas@gmail.com>
2023-06-27 14:53:04 +0100

    feat: support passing file paths as input

---
+++ demo/add.monkey
@@ -0,0 +1,8 @@
+let a = 5;
+let b = 8;
+
+let add = fn(a,b) {
+  return a + b;
+};
+
+puts(add(a,b));
--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,36 @@ mod object;
 mod parser;
 mod repl;
 
+use std::{cell::RefCell, env, fs, rc::Rc};
+
+use parser::Parser;
 use repl::Repl;
 
+use crate::object::Environment;
+
 fn main() {
-    let mut repl = Repl::new();
-    match repl.start() {
-        Ok(_) => (),
-        Err(e) => println!("{}", e),
-    };
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() < 2 {
+        let mut repl = Repl::new();
+        match repl.start() {
+            Ok(_) => (),
+            Err(e) => println!("{}", e),
+        };
+        return;
+    }
+
+    let file_path = &args[1];
+    let contents = fs::read_to_string(file_path).expect("Should have been able to read the file");
+    let mut parser = Parser::from_source(&contents);
+    let program = parser.parse_program();
+
+    if parser.errors.is_empty() {
+        eval::eval(program, Rc::new(RefCell::new(Environment::new())));
+    } else {
+        println!("Parsing Failed");
+        for error in parser.errors {
+            println!("  - {}", error);
+        }
+    }
 }
